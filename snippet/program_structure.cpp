b3c430245b5950769f064f804c1751d961feb118
Yan Zhou <zhouyan@me.com>
2013-09-27 00:28:06 +0100

    section on parallel computing

--- snippet/program_structure.cpp
+++ snippet/program_structure.cpp
@@ -1,11 +1,15 @@
 int main ()
 {
     Sampler<T> sampler(N, Stratified, 0.5);
+
     sampler.init(init_f);             // Step 1.
     sampler.move(move_f, false);      // Step 2.(a)
     sampler.mcmc(mcmc_f1, true);      // Step 2.(c)
     sampler.mcmc(mcmc_f2, true);      // Step 2.(c)
-    sampler.path_sampling(path_eval); // Path sampling
+
+    // Path sampling
+    sampler.path_sampling(path_eval);
+
     // Importance sampling estimates of moments
     sampler.monitor("moments", 2, moments_eval);
 
@@ -13,7 +17,12 @@ int main ()
     sampler.initialize(param);
     sampler.iterate(IterNum);
 
-    // Output to file
+    // Results of path sampling
+    std::cout << "Path sampling: " << std::endl;
+    std::cout << sampler.path_sampling() << std::endl;
+
+    // Output importance sampling approximation
+    // and other aspects of the history of the sampler
     std::ofstream output("sampler_file");
     output << sampler << std::endl;
     output.close();
--- snippet/smc_move.cpp
+++ snippet/smc_move.cpp
@@ -1,10 +1,13 @@
 template <typename T>
 std::size_t smc_move (std::size_t iter, Particle<T> &particle)
 {
+    // Calculate $\alpha_{\mathrm{diff}} = \alpha(t/T) - \alpha([t-1]/T)$
     const double alph_diff =
         particle.value().alpha(iter) -
         particle.value().alpha(iter - 1);
 
+    // Calculate the logarithm of the incremental weights
+    // $\tilde{w}_t = \alpha_{\mathrm{diff}} \log p(\data|\theta_t,\calM)$
     std::vector<double> log_inc_w(particle.size());
     for (std::size_t i = 0; i != particle.size(); ++i) {
         log_inc_w[i] =
